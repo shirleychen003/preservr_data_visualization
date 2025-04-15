b4dee8876fc5ee25733635c7bbfd05b331bbb5ef
Luca Carnegie <41924665+lcarnegie@users.noreply.github.com>
2025-04-15 17:41:00 -0400

    change ui to be prettier

---
+++ main.py
@@ -0,0 +1,5 @@
+from ui.project_ui import InstagramArchiveApp
+
+if __name__ == "__main__":
+    app = InstagramArchiveApp()
+    app.mainloop()
--- ui/__init__.py
+++
@@ -1,8 +0,0 @@
-import tkinter as tk
-from project_ui import InstagramArchiveApp
-
-# Run the application
-if __name__ == "__main__":
-    root = tk.Tk()
-    app = InstagramArchiveApp(root)
-    root.mainloop()
